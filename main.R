34e30fc4778d4e6e006fe28daa32d3d3ae32391a
AijayI <167138962+AijayI@users.noreply.github.com>
2024-04-15 16:10:38 -0600

    filtered to those above 30

--- main.R
+++ main.R
@@ -1,7 +1,12 @@
 library(tidyverse)
 library(here)
+library(dplyr)
 
 
-tv_hrs_tab <- gss_cat %>% 
+tv_hrs_tab <- gss_cat %>%
+  filter(age >=30) %>%
   group_by(marital) %>% 
   summarise(mean_tv_hrs = mean(tvhours, na.rm = T))
+write_csv(tv_hrs_tab, here("TV_Hours_By_Marital"))
+
+tv_hrs_tab
