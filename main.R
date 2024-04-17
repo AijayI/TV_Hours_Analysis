c0a5e97735a05e51f50ebd1db8aebff12fc6c8c5
AijayI <167138962+AijayI@users.noreply.github.com>
2024-04-16 22:38:51 -0600

    filter to those under 30

--- main.R
+++ main.R
@@ -4,7 +4,7 @@ library(dplyr)
 
 
 tv_hrs_tab <- gss_cat %>%
-  filter(age >=30) %>%
+  filter(age <=30) %>%
   group_by(marital) %>% 
   summarise(mean_tv_hrs = mean(tvhours, na.rm = T))
 write_csv(tv_hrs_tab, here("TV_Hours_By_Marital"))
